95778f7033adc9ab13e2e18ecfb920ccb67a75d4
TheBossMagnus <TheBossMagnus@proton.me>
2025-03-24 09:55:07 +0100

    salva in json
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- src/client/get_data.py
+++ src/client/get_data.py
@@ -2,13 +2,14 @@ import os
 import psutil
 import requests
 import socket
+import json
 
 
 def organizza_dati():
-    data = {"hostname": get_hostname(),
-            "username": get_logged_in_user(), 
-            "info": {"cpu_temperature": get_cpu_temperature(), "internet_connection": is_connected_to_internet(), "ram_usage": get_ram_usage()}}
-    return data
+    # Return all data directly without nesting under 'info'
+    data = {"username": get_logged_in_user(), "cpu_temperature": get_cpu_temperature(), "internet_connection": is_connected_to_internet(), "ram_usage": get_ram_usage()}
+    return json.dumps({"hostname": get_hostname(), "data": data})
+
 
 def get_hostname():
     return socket.gethostname()
--- src/server/gestore_bluetooth.py
+++ src/server/gestore_bluetooth.py
@@ -1,12 +1,14 @@
 import threading
-
+import time
 import bluetooth
+import json
+import os
 
 from config import (
-    BLUETOOTH_DEVICE_NAME,
-    BLUETOOTH_UUID,
+    NOME_DISPOSITIVO_BLUETOOTH,
+    UUID_BLUETOOTH,
     DIMENSIONE_BUFFER,
-    MAX_CONNECTIONS,
+    CONNESSIONI_MASSIME,
     TIMEOUT,
 )
 
@@ -24,19 +26,19 @@ def avvia_server():
     # Inizializza il socket del server
     socket_server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     socket_server.bind(("", 3))
-    socket_server.listen(MAX_CONNECTIONS)
+    socket_server.listen(CONNESSIONI_MASSIME)
     porta = socket_server.getsockname()[1]
 
     # Pubblicizza il servizio
     bluetooth.advertise_service(
         socket_server,
-        BLUETOOTH_DEVICE_NAME,
-        service_id=BLUETOOTH_UUID,
-        service_classes=[BLUETOOTH_UUID, bluetooth.SERIAL_PORT_CLASS],
+        NOME_DISPOSITIVO_BLUETOOTH,
+        service_id=UUID_BLUETOOTH,
+        service_classes=[UUID_BLUETOOTH, bluetooth.SERIAL_PORT_CLASS],
         profiles=[bluetooth.SERIAL_PORT_PROFILE],
     )
 
-    print(f"Server Bluetooth '{BLUETOOTH_DEVICE_NAME}' avviato su RFCOMM {porta}")
+    print(f"Server Bluetooth '{NOME_DISPOSITIVO_BLUETOOTH}' avviato su RFCOMM {porta}")
     print("Premi Ctrl+C per fermare il server")
 
     in_esecuzione = True
@@ -45,9 +47,7 @@ def avvia_server():
         while in_esecuzione:
             # Accetta e gestisci connessioni
             socket_client, info_client = socket_server.accept()
-            thread = threading.Thread(
-                target=gestisci_client, args=(socket_client, info_client)
-            )
+            thread = threading.Thread(target=gestisci_client, args=(socket_client, info_client))
             thread.daemon = True
             thread.start()
     except KeyboardInterrupt:
@@ -83,14 +83,31 @@ def gestisci_client(socket_client, info_client):
             if socket_client in connessioni:
                 connessioni.remove(socket_client)
         socket_client.close()
-        print(
-            f"Client {info_client} disconnesso. Clienti rimanenti: {len(connessioni)}"
-        )
+        print(f"Client {info_client} disconnesso. Clienti rimanenti: {len(connessioni)}")
 
 
 def elabora_dati(dati, info_client):
-    """Elabora i dati ricevuti da un client"""
-    print(f"Dati da {info_client}: {dati}")
+    """Elabora i dati ricevuti dal client e li salva in formato JSON"""
+
+    # Log i dati ricevuti
+    with open("log.txt", "a") as file:
+        ora = time.strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{ora}] {info_client}: {dati}\n")
+
+    dati = json.loads(dati)
+    hostnameNuoviDati = dati["hostname"]
+    # Leggi il database esistente
+    with open("database.json", "r") as file:
+        database = json.loads(file.read())
+    if hostnameNuoviDati in database:  # Controlla se il dispositivo esiste già nel database, aggiorna i dati
+        database["dispositivi"][hostnameNuoviDati] = dati.get("data", {})
+    else:
+        # Aggiungi un nuovo dispositivo al database
+        file.append(dati.json())
+
+    # Salva il database aggiornato
+    with open("database.json", "w") as file:
+        file.write(json.dumps(database))
 
 
 def chiudi_tutto():
