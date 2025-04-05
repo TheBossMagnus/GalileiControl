369f42f904bd36220933b7d1622f1b2f3bf15b44
TheBossMagnus <TheBossMagnus@proton.me>
2025-04-05 12:33:03 +0200

    fix dash
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- src/dashboard/dashboard.ino
+++ src/dashboard/dashboard.ino
@@ -1,16 +1,22 @@
 const int ledPins[3][3] = {
-    {2, 3, 4}, // LED RGB 0
-    {5, 6, 7}, // LED RGB 1
-    {8, 9, 10} // LED RGB 2
+    {3, 4, 5}, // LED  0
+    {6, 7, 8}, // LED  1
+    {9, 10, 11} // LED  2
 };
 
 void setup() {
     Serial.begin(9600); // Inizializza la comunicazione seriale
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
-            pinMode(ledPins[i][j], OUTPUT); // Imposta i pin dei LED RGB come output
+            pinMode(ledPins[i][j], OUTPUT); // Imposta i pin dei LED YMC come output
         }
     }
+}
+void rgbLedDisplay(int ledPinR, int ledPinG, int ledPinB, int red, int green, int blue) {
+// Set three ledPin to output the PWM duty cycle
+analogWrite(ledPinR, constrain(red, 0, 255));
+analogWrite(ledPinG, constrain(green, 0, 255));
+analogWrite(ledPinB, constrain(blue, 0, 255));
 }
 
 void loop() {
@@ -18,17 +24,21 @@ void loop() {
         String input = Serial.readStringUntil('\n'); // Legge la stringa fino al carattere di nuova riga
         input.trim(); // Rimuove spazi bianchi iniziali o finali se presenti
         
-        // Estrae i valori dalla stringa ricevuta e li converte ad int (es: "1.200.200.200" -> LED 1, R 200, G 200, B 200)
+        // Estrae i valori dalla stringa ricevuta e li converte ad int (es: "1.200.200.200" -> LED 1, Y 200, M 200, C 200)
         int indiceLed = input.substring(0, 1).toInt(); // Identifica quale LED controllare
-        int r = input.substring(2, 5).toInt(); // Estrae il valore R
-        int g = input.substring(6, 9).toInt(); // Estrae il valore G
-        int b = input.substring(10, 13).toInt(); // Estrae il valore B
+        int r = input.substring(2, 5).toInt(); // Estrae il valore C
+        int g = input.substring(6, 9).toInt(); // Estrae il valore M
+        int b = input.substring(10, 13).toInt(); // Estrae il valore Y
+      
+        Serial.print(r);
+        Serial.print(g);
+        Serial.print(b);
+        
         
         // Controlla se l'indice del LED è valido (0, 1 o 2) e imposta il colore corretto al LED stesso
         if(indiceLed>=0 && indiceLed<3){
-            analogWrite(ledPins[indiceLed][0], r); // Imposta il valore del rosso
-            analogWrite(ledPins[indiceLed][1], g); // Imposta il valore del verde
-            analogWrite(ledPins[indiceLed][2], b); // Imposta il valore del blu
+            rgbLedDisplay(ledPins[indiceLed][0], ledPins[indiceLed][1], ledPins[indiceLed][2], r, g, b); // Imposta il valore del rosso
+ 
         }
     }
 }
